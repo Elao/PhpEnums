bc2542ad95bde2a1922ff3fecedfc8cf6b1ad4f1
xavier-rdo <xavier.roldo@elao.com>
2017-06-22 10:51:31 +0200

    Integration test for Bridge/Faker/EnumProvider

---
+++ tests/Fixtures/EnumAware/SimpleEntity.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the "elao/enum" package.
+ *
+ * Copyright (C) 2016 Elao
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Elao\Enum\Tests\Fixtures\EnumAware;
+
+use Elao\Enum\Tests\Fixtures\Enum\Permissions;
+use Elao\Enum\Tests\Fixtures\Enum\SimpleEnum;
+
+class SimpleEntity
+{
+    /** @var Permissions */
+    public $permissions;
+
+    /** @var SimpleEnum */
+    public $simpleEnum;
+}

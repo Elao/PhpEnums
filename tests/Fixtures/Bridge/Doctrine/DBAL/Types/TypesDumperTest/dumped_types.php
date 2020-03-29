eaddbfa5751ab66c75ca816ccf2bae493c9f8201
Indra Gunawan <guind.online@gmail.com>
2020-03-30 02:02:33 +0700

    group generated type class by namespace

--- tests/Fixtures/Bridge/Doctrine/DBAL/Types/TypesDumperTest/dumped_types.php
+++ tests/Fixtures/Bridge/Doctrine/DBAL/Types/TypesDumperTest/dumped_types.php
@@ -1,39 +1,60 @@
 <?php
 
-namespace ELAO_ENUM_DT\Foo\Bar;
+namespace ELAO_ENUM_DT\Foo\Bar {
 
-if (!\class_exists('\ELAO_ENUM_DT\Foo\Bar\BazType')) {
-    class BazType extends \Elao\Enum\Bridge\Doctrine\DBAL\Types\AbstractEnumType
-    {
-        const NAME = 'baz';
-
-        protected function getEnumClass(): string
+    if (!\class_exists(\ELAO_ENUM_DT\Foo\Bar\BazType::class)) {
+        class BazType extends \Elao\Enum\Bridge\Doctrine\DBAL\Types\AbstractEnumType
         {
-            return \Foo\Bar\Baz::class;
-        }
+            const NAME = 'baz';
 
-        public function getName(): string
-        {
-            return static::NAME;
+            protected function getEnumClass(): string
+            {
+                return \Foo\Bar\Baz::class;
+            }
+
+            public function getName(): string
+            {
+                return static::NAME;
+            }
         }
     }
+
+    if (!\class_exists(\ELAO_ENUM_DT\Foo\Bar\QuxType::class)) {
+        class QuxType extends \Elao\Enum\Bridge\Doctrine\DBAL\Types\AbstractIntegerEnumType
+        {
+            const NAME = 'qux';
+
+            protected function getEnumClass(): string
+            {
+                return \Foo\Bar\Qux::class;
+            }
+
+            public function getName(): string
+            {
+                return static::NAME;
+            }
+        }
+    }
+
 }
 
-namespace ELAO_ENUM_DT\Foo\Bar;
+namespace ELAO_ENUM_DT\Foo\Baz {
 
-if (!\class_exists('\ELAO_ENUM_DT\Foo\Bar\QuxType')) {
-    class QuxType extends \Elao\Enum\Bridge\Doctrine\DBAL\Types\AbstractIntegerEnumType
-    {
-        const NAME = 'qux';
-
-        protected function getEnumClass(): string
+    if (!\class_exists(\ELAO_ENUM_DT\Foo\Baz\FooType::class)) {
+        class FooType extends \Elao\Enum\Bridge\Doctrine\DBAL\Types\AbstractIntegerEnumType
         {
-            return \Foo\Bar\Qux::class;
-        }
+            const NAME = 'foo';
 
-        public function getName(): string
-        {
-            return static::NAME;
+            protected function getEnumClass(): string
+            {
+                return \Foo\Baz\Foo::class;
+            }
+
+            public function getName(): string
+            {
+                return static::NAME;
+            }
         }
     }
+
 }

57f5986777a263668dc71beece54f1218b009b1a
Maxime Steinhausser <maxime.steinhausser@elao.com>
2026-04-13 13:29:03 +0200

    Deprecate BackedEnumFromQuery in favor of Symfony's MapQueryParameter
    The `#[BackedEnumFromQuery]` attribute is deprecated since 2.6 and will
    be removed in 3.0. Users should migrate to Symfony's native
    `#[MapQueryParameter]` attribute (available since Symfony 6.3).
    
    - Add trigger_deprecation() in BackedEnumFromQuery constructor
    - Update README to recommend MapQueryParameter for query params
    - Add dedicated BackedEnumFromBody documentation with examples
    - Split unit/integration tests into query (legacy) and body groups
    - Add test verifying the deprecation message is triggered

--- src/Bridge/Symfony/HttpKernel/Controller/ArgumentResolver/Attributes/BackedEnumFromQuery.php
+++ src/Bridge/Symfony/HttpKernel/Controller/ArgumentResolver/Attributes/BackedEnumFromQuery.php
@@ -12,7 +12,19 @@ declare(strict_types=1);
 
 namespace Elao\Enum\Bridge\Symfony\HttpKernel\Controller\ArgumentResolver\Attributes;
 
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+
 #[\Attribute(\Attribute::TARGET_PARAMETER)]
 final class BackedEnumFromQuery
 {
+    public function __construct()
+    {
+        trigger_deprecation(
+            'elao/enum',
+            '2.6',
+            'The "%s" attribute is deprecated, use "%s" instead. It will be removed in 3.0.',
+            self::class,
+            MapQueryParameter::class,
+        );
+    }
 }
